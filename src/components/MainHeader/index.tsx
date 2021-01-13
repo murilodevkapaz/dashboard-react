6ba90842a3e443df7d9d8985c250c4fd46dd4f00
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2021-01-13 06:51:32 -0300

    emoji included in header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
-import {Container} from './styles';
+import {Container, Profile, Welcome, UserName} from './styles';
+import emojis from '../../utils/emojis';
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(()=>{
+
+        const i = Math.floor(Math.random()*emojis.length);
+        return emojis[i];
+    }, []);
+
     return (
         <Container>
             <h1>MainHeader</h1>
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Murilo Rerison</UserName>
+            </Profile>
         </Container>
     )
 }
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -2,6 +2,19 @@ import styled from 'styled-components';
 
 export const Container  = styled.div`
     grid-area: MH;
-    color: ${props=> props.theme.colors.white};
+    /* color: ${props=> props.theme.colors.white}; */
     background-color: ${props =>props.theme.colors.secondary};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+    border-bottom: 1px solid ${props=>props.theme.colors.gray};
 `;
+
+export const Profile = styled.div`
+    color: ${props=>props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3``;
+
+export const UserName = styled.span``;
