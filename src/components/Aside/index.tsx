52cbce1be2015ab2904f4ae05389a9be37de6c8d
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2021-01-15 06:46:32 -0300

    list aside finished

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {
+    MdDashboard,
+    MdArrowUpward,
+    MdArrowDownward,
+    MdExitToApp
+} from 'react-icons/md'
 
-import {Container} from './styles';
+import {
+    Container,
+    LogImg,
+    MenuContainer,
+    Header,
+    MenuItemLink,
+    Title
+} from './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogImg src={logoImg} alt="Logo da Minha Carteira" />
+                <Title>MinhaCarteira</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#"><MdDashboard/>Dashboard</MenuItemLink>
+                <MenuItemLink href="#"><MdArrowUpward/>Entradas</MenuItemLink>
+                <MenuItemLink href="#"><MdArrowDownward/>Saídas</MenuItemLink>
+                <MenuItemLink href="#"><MdExitToApp/>Sair</MenuItemLink>
+            </MenuContainer>
         </Container>
     )
 }
--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -2,7 +2,7 @@ import React, {useMemo} from 'react';
 
 import {Container, Profile, Welcome, UserName} from './styles';
 import emojis from '../../utils/emojis';
-import Toggle from '../Toggle';
+// import Toggle from '../Toggle';
 
 const MainHeader: React.FC = () => {
 
@@ -14,7 +14,7 @@ const MainHeader: React.FC = () => {
 
     return (
         <Container>
-            <Toggle />
+            {/* <Toggle /> */}
             <h1>MainHeader</h1>
             <Profile>
                 <Welcome>Olá, {emoji}</Welcome>
