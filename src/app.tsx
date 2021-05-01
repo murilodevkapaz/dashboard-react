f093743987e090d9231de0d045c245f6c2c77f3b
murilokapaz <murilodevkapaz@gmail.com>
2021-05-01 14:12:38 -0300

    starting  theme change

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
 
 import GlobalStyles from './styles/globalStyles';
-import dark from './styles/themes/dark';
+
+
+import {useTheme} from './hooks/theme';
 
 //o ThemeProvider escolhe qual o tema da página usando o arquivo 'styled.d.ts' e definindo os temas dark e light
 
 const App: React.FC = () => {
+    const {theme} = useTheme();
     return (
-        <ThemeProvider theme={dark}>
+        <ThemeProvider theme={theme}>
             <GlobalStyles />
             <Routes />
         </ThemeProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './app';
 
+import { ThemeProvider } from './hooks/theme'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
