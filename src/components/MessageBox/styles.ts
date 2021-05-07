3c94f61285eef5050ed32f40f5de2a80e5ff2dab
murilokapaz <murilodevkapaz@gmail.com>
2021-05-07 09:42:12 -0300

    continuing responsivity in the cards

--- src/components/MessageBox/styles.ts
+++ src/components/MessageBox/styles.ts
@@ -23,4 +23,29 @@ export const Content = styled.div`
     > header p{
         font-size: 18px;
     }
+
+    @media(max-width: 770px){
+        width: 100%;
+        >header h1{
+            font-size: 24px;
+
+            img{
+                height: 20px;
+                width: 20px;
+            }
+            >header p,
+            >footer span{
+                font-size: 14px;
+            }
+        }
+    }
+
+    @media(max-width: 420px){
+        width: 100%;
+        height: auto;
+
+        >header p{
+            margin-bottom: 15px;
+        }
+    }
 `;
